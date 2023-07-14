10251de8e50120a670d6c25bcaf9cdbebbf7fb60
Haider <haider.g@erasolution.ae>
2023-07-14 17:25:03 +0400

    Added ft_bzero + ft_memset

---
+++ ft_bzero.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+void ft_bzero(void *str, size_t n)
+{
+    if (str != NULL && n > 0)
+    {
+
+        char *ptr = (char *)str;
+        size_t i;
+        i = 0;
+        while (i < n)
+        {
+            ptr[i] = 0;
+            i++;
+        }
+    }
+}
+
+#include <strings.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // char *test = malloc(1);
+    // bzero(test, 5);
+
+    char *t = malloc(4);
+    ft_bzero(t, 5);
+}
---
+++ ft_memset.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+void ft_memset(void *str, int c ,size_t n)
+{
+    if (str != NULL && n > 0)
+    {
+
+        char *ptr = (char *)str;
+        size_t i;
+        i = 0;
+        while (i < n)
+        {
+            ptr[i] = c;
+            i++;
+        }
+    }
+}
+
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+    char test[50];
+    strcpy(test,"This is string.h library function");
+    puts(test);
+    memset(test, '$' ,60);
+    puts(test);
+
+    char t[50];
+    strcpy(t,"This is string.h library function");
+    puts(t);
+    ft_memset(t,'$',60);
+    puts(t);
+}
