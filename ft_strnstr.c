99b0eddc7d57a9614639855fb9f7c6680d8a6465
Chief <haydarguetta@gmail.com>
2023-08-03 18:45:45 +0400

    Add

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,5 +1,16 @@
 #include <stdlib.h>
 
+int    ft_strlen(char *str)
+{
+    int    i;
+    i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return (i);
+}
+
 char * ft_strnstr(const char *big, const char *little, size_t len)
 {
     if (*little == '\0')
